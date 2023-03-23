64ed2ecd0d5b05a2b23aeefb83215583c86f375e
LonelyFellas <289582304@qq.com>
2023-03-23 20:28:29 +0800

    例子6完成✅

---
+++ 例子6/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+  printf("请你用*打印出字母C: \n");
+  printf("    ************\n");
+  printf("   ************\n");
+  printf("  **\n");
+  printf(" **\n");
+  printf("**\n");
+  printf("**\n");
+  printf("**\n");
+  printf("**\n");
+  printf("**\n");
+  printf(" **\n");
+  printf("  **\n");
+  printf("   ************\n");
+  printf("    ************\n");
+}
